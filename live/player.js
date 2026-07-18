c146f4f9adbc2aaed2c20d375a7331e2ad6b3dcd
Wawat31 <129237106+Wawat31@users.noreply.github.com>
2026-07-18 23:45:41 +0700

    Update player.js

--- live/player.js
+++ live/player.js
@@ -1,4 +1,4 @@
-const stream1 = "https://livecdn.euw1-0008.jwplive.com/live/sites/fM9jRrkn/media/tMiWEsHp/live.isml/.m3u8";
+const stream1 = "https://live.kinxie.com/live/71943770_7d79c45badd293030f8ef83e91fa3d69_1080p.m3u8?auth_key=1784401340-0-0-e74d018ae7807eb9d5b6c802950747a5";
 const stream2 = "https://live.666666.zip/live/4523585.m3u8";
 const stream3 = "LINK_M3U8_3";
 
